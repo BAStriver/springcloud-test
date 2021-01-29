98ce93f257c7a21ce8bd27f13144866427a57361
czhang20 <1312395217@qq.com>
2021-01-29 18:03:00 +0800

    update Gateway demo

---
+++ cloud-gateway-gateway9527/src/main/java/com/bas/springcloud/GatewayMain9527.java
@@ -0,0 +1,21 @@
+package com.bas.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @title: fidb-parse
+ * @description:
+ * @author: Carlos.Zhang@morningstar.com
+ * @create: 2021/1/29 17:19
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
+
